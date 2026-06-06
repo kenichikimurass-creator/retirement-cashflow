707527fc03b94eca3da1faf98a3bfde1f99ac159
kenichikimurass-creator <kenichi.kimura.ss@gmail.com>
2026-06-06 17:36:39 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'retirement-cashflow-v8-1';
+const CACHE_NAME = 'retirement-cashflow-v9-1';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
